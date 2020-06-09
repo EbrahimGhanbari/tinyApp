5e3c5a8fe2337c3252216eec42513cf4f33949a1
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-09 05:38:04 +0000

    starting part 2 of homework

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const generateRandomString = require("./generateRandomString");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +17,15 @@ let test;
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
---
+++ generateRandomString.js
@@ -0,0 +1,12 @@
+const generateRandomString = () => {
+  let result = "";
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for ( var i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  
+  return result;;
+};
+
+module.exports = generateRandomString;
