71bd3871c9377ac7ecc828180f1c2a3b1b9e0371
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-10 07:14:50 +0000

    the login section almost working

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const generateRandomString = require("./generateRandomString");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -13,15 +15,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+let templateVars = {
+  urls: urlDatabase,
+  username: ""
+};
+
+app.post("/login", (req, res) => {
+  templateVars['username'] = "";
+  res.cookie('username', req.body.username);
+  templateVars['username'] = req.cookies["username"];
+  res.redirect('/urls');
+
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', templateVars['username'])
+  templateVars['username'] = "";
+  res.redirect('/urls');
+
+});
+
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", templateVars); 
 });
 
+// Add random charactars for each websit
 app.post("/urls", (req, res) => {
   let key = generateRandomString();
   urlDatabase[key]= `http://${req.body['longURL']}`;
@@ -42,7 +66,6 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect(`/urls/${req.params.shortURL}`);
 });
 
-
 // Delete button
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
@@ -50,13 +73,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 });
 
-
-
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
